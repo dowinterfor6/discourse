0ff36f342b64e44601fd1dddc526b0399c16f438
dowinterfor6 <dowinterfor6@gmail.com>
2019-04-30 15:11:16 -0700

    Complete core reducers and install redux-logger/thunk

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 // TESTING STUFF HERE
 import * as sessionApiUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(<h1>REACT IS WORKING YAY</h1>, root);
 
@@ -11,4 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
   window.login = sessionApiUtil.login;
   window.logout = sessionApiUtil.logout;
   window.signup = sessionApiUtil.signup;
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
