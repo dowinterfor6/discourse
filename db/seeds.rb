5f95b22d845ddf699b91682d2d7681987e3d96df
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-03 11:27:15 -0700

    Add basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-User.create!(username: 'username', email:'user@gmail.com', password: 'password')
+User.create!(id: 1, username: 'demo_user', email:'demo@gmail.com', password: 'password')
+User.create!(id: 2, username: 'dowinterfor6', email:'winter@gmail.com', password: 'dowinterfor6')
+User.create!(id: 3, username: 'username', email:'user@gmail.com', password: 'password')
+User.create!(id: 4, username: 'billybob', email:'billybob@gmail.com', password: 'password')
+User.create!(id: 5, username: 'ginger_baker', email:'gbaker@gmail.com', password: 'password')
+
+Server.destroy_all
+Server.create!(id: 1, name: "Demo user's amazing server", owner_id: 1)
+Server.create!(id: 2, name: 'Elder Memes Online', owner_id: 2)
+Server.create!(id: 3, name: "Bakers' hangout", owner_id: 5)
+
+UserServer.destroy_all
+UserServer.create!(user_id: 1,server_id: 1)
+UserServer.create!(user_id: 2,server_id: 1)
+UserServer.create!(user_id: 3,server_id: 1)
+UserServer.create!(user_id: 4,server_id: 1)
+UserServer.create!(user_id: 5,server_id: 1)
+
+UserServer.create!(user_id: 2,server_id: 2)
+UserServer.create!(user_id: 4,server_id: 2)
+UserServer.create!(user_id: 5,server_id: 2)
+
+UserServer.create!(user_id: 5,server_id: 3)
+UserServer.create!(user_id: 1,server_id: 3)
+UserServer.create!(user_id: 3,server_id: 3)
