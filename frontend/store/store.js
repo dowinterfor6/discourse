e25ec00235cbd72db67986f727de3d2aba2c9967
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-03 09:35:39 -0700

    Remove some activity content, added conditional for store to not add logger as middleware if in production mode

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,12 +3,16 @@ import rootReducer from "../reducers/root_reducer";
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
-const configureStore = (preloadedState = {}) => (
-  createStore(
+const configureStore = (preloadedState = {}) => {
+  let middleware = [thunk];
+  if (process.env.NODE_ENV !== 'production') {
+    middleware = [...middleware, logger];
+  }
+  return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
-  )
-);
+    applyMiddleware(...middleware)
+  );
+};
 
 export default configureStore
