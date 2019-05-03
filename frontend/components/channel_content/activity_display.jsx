f5983b64f13ebd666fae133f2f03a40d19cc6973
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-02 20:53:45 -0700

    Add some activity content and styling

--- frontend/components/channel_content/activity_display.jsx
+++ frontend/components/channel_content/activity_display.jsx
@@ -3,8 +3,21 @@ import React from 'react';
 const ActivityDisplay = () => (
   <section className="activity-display-container" scrolling="no">
     <ul className="activities-display-list">
-      <li>Fun stuff</li>
-      <li>More fun stuff</li>
+      <li>
+        <iframe className="activity-video-content" width="560" height="315" src="https://www.youtube.com/embed/RrjJtYpOawU?start=61&controls=0&mute=1&autoplay=1" frameBorder="0"></iframe>
+        <div className="activity-content-text">
+          In Fallout 76, the first nuke is launched and it has a huge effect: 
+          deathclaws get wings, the world is thrown into a nuclear winter, 
+          and technology is set back to the Iron Age. Your player wakes up in a cart
+          with his hands bound, greeted by the fresh smell of pine and the voices 
+          of a few other men. "Hey you, finally awake? You were trying to cross 
+          the border right?" one of them asks, as a title appears in the sky: 
+          "Elder Scrolls V: Skyrim".
+        </div>    
+      </li>
+      <li>
+        <img className="activity-photo-content" src="https://cdn.wccftech.com/wp-content/uploads/2016/09/Half-Life-3.jpg" alt="Halflife 3"/>
+      </li>
       <li>Very creative fun stuff</li>
       <li>Honestly running out of ideas</li>
       <li>Half Life 3</li>
