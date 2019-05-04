828f65d6508003967b5631d4297bb9a09bc07f6a
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-04 15:34:25 -0700

    Remove console log of function

--- frontend/components/discord_servers/add_server_icon.jsx
+++ frontend/components/discord_servers/add_server_icon.jsx
@@ -4,7 +4,7 @@ class AddServerIcon extends React.Component {
 
   render() {
     return (
-      <div className="add-server-icon" onClick={()=> console.log(this.props)}>+</div>
+      <div className="add-server-icon">+</div>
     )
   }
 }
