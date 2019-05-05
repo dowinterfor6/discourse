a7ad13044c318ecfb2c73aa24fc1f44aac1700c7
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-04 19:43:42 -0700

    Add basic modal styling

--- frontend/components/modal/server_modal.jsx
+++ frontend/components/modal/server_modal.jsx
@@ -4,7 +4,24 @@ class ServerModal extends React.Component {
 
   render() {
     return (
-      <h1>HAI DIS IS SERVERMODAL SPEAKING</h1>
+      <div className="modal-form-index">
+        <h1>Oh, another server huh?</h1>
+        <div className="modal-form-container">
+          <div className="modal-link-container create">
+            <h2>Create</h2>
+            <h5>Create a new server and invite your friends, it's free!</h5>
+            <img src="" alt="placeholder"/>
+            <button>Create a server</button>
+          </div>
+          <label>or</label>
+          <div className="modal-link-container join">
+            <h2>Join</h2>
+            <h5>Enter an instant invite and join your friend's server</h5>
+            <img src="" alt="placeholder" />
+            <button>Join a server</button>
+          </div>
+        </div>
+      </div>
     )
   }
 }
