a4f0acf279e6666c84ccad0746f6e22551e513e7
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-07 11:47:43 -0700

    Fix edge case where leaving a server required two actions

--- app/controllers/api/user_servers_controller.rb
+++ app/controllers/api/user_servers_controller.rb
@@ -1,6 +1,6 @@
 class Api::UserServersController < ApplicationController
   def destroy
-    @user_server = UserServer.find_by(server_id: params[:id])
+    @user_server = UserServer.find_by(server_id: params[:id], user_id: current_user.id)
     if @user_server
       @user_server.delete
       @server = Server.find_by(id: params[:id])
