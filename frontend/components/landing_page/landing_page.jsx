34d112cf0d9dd9f65597d19a5ce74d6397ddfd75
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-09 10:52:17 -0700

    Tidy up front page, remove dead links, added proper links

--- frontend/components/landing_page/landing_page.jsx
+++ frontend/components/landing_page/landing_page.jsx
@@ -6,29 +6,46 @@ const landingPage = () => (
     <nav className="nav-bar">
       <ul className="left-nav">
         <li><img src={window.discordLogoURL} alt="Discourse Logo"></img></li>
-        <li>Download</li>
+        {/* <li>Download</li>
         <li>Nitro</li>
         <li>Jobs</li>
         <li>Developers</li>
         <li>Community</li>
-        <li>Support</li>
+        <li>Support</li> */}
       </ul>
       <ul className="right-nav">
-        <li><i className="fab fa-twitter fa-lg"></i></li>
+        {/* <li><i className="fab fa-twitter fa-lg"></i></li>
         <li><i className="fab fa-facebook-square fa-lg"></i></li>
-        <li><i className="fab fa-instagram fa-lg"></i></li>
-        <li><Link to='/login' className="login-link">Login</Link></li>
-        <li>Language</li>
+        <li><i className="fab fa-instagram fa-lg"></i></li> */}
+        <li>
+          <a href="https://github.com/dowinterfor6">
+            <i className="fab fa-github-square fa-2x"></i>  
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/andrew-chan-b6ab12168/">
+            <i className="fab fa-linkedin fa-2x"></i>
+          </a>
+        </li>
+        <li className="login-link-container">
+          <Link to='/login' className="login-link">Login</Link>
+        </li>
+        {/* <li>Language</li> */}
       </ul>
     </nav>
 
     <section className="main-content">
-      <h1>It's time to ditch Skype and Teamspeak.</h1>
-      <p>All-in-one voice and text chat for gamers that's free, secure, and works on both your desktop and phone. Stop paying for
-      TeamSpeak servers and hassling with Skype. Simplify your life.</p>
+      <h1>It's time to ditch Scype and Teamspoke.</h1>
+      <p>Nothing-in-one voice and text chat for gamers that's free, insecure, and works 
+      (mostly). Stop paying for TeamSpoke servers and hassling with Scype. Complicate your 
+      life with Discourse.</p>
       <div id="content-buttons">
-        <button>Download for Linux</button>
-        <button>Open Discord in your Browser</button>
+        <a href="https://github.com/dowinterfor6/discourse">
+          <button>Open Discourse on Github</button>
+        </a>
+        <a href="https://discord.gg">
+          <button>Go to Discord homepage</button>
+        </a>
       </div>
     </section>
 
@@ -43,29 +60,23 @@ const landingPage = () => (
             <img src={window.discordSmallLogoURL} alt="Discourse Small Logo"></img>
           </ul>
           <ul id="foot-header">
-            <li id="header">Product</li>
-            <li>Download</li>
-            <li>Branding</li>
-            <li>Nitro</li>
+            <li id="header">Goal</li>
+            <li>Discord clone</li>
           </ul>
           <ul id="foot-header">
-            <li id="header">Developers</li>
-            <li>Sell Your Game</li>
-            <li>Rich Presence</li>
-            <li>Verification</li>
-            <li>Applications</li>
-            <li>Documentation</li>
+            <li id="header">Technologies</li>
+            <li>Javascript frontend</li>
+            <li>React/Redux libraries</li>
+            <li>PostgreSQL database</li>
+            <li>Ruby on Rails backend</li>
           </ul>
           <ul id="foot-header">
-            <li id="header">Resources</li>
-            <li>Help & Support</li>
-            <li>Guidelines</li>
-            <li>Feedback</li>
-            <li>Terms</li>
-            <li>Privacy</li>
-            <li>Status</li>
+            <li id="header">Open source assets</li>
+            <li>Font Awesome icons</li>
+            <li>Discord color palette</li>
+            <li>Google Fonts</li>
           </ul>
-          <ul id="foot-header">
+          {/* <ul id="foot-header">
             <li id="header">Company</li>
             <li>About</li>
             <li>Blog</li>
@@ -78,14 +89,14 @@ const landingPage = () => (
             <li>Merch Store</li>
             <li>Press Inquiries</li>
             <li>Open Source</li>
-          </ul>
+          </ul> */}
         </ul>
       </section>
 
       <section className="below-footer">
         <div id="left-footer">
           <h2>Ready to try Discourse? It's free!</h2>
-          <p>Join over 250 million players today</p>
+          <p>Join over 0.25 players today</p>
         </div>
         <div id="right-footer">
           <Link to='/signup'><button>Sign Up Now</button></Link>
